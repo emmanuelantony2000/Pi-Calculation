e6c103296ff07f6f70cb3d9d20c8d462811d170e
Emmanuel Antony <emmanuelantony2000@gmail.com>
2017-06-20 23:24:52 +0530

    Add files via upload
    Corrected the formula for the calculation.

--- Code/PiCalculationWithSinInverseExpansion.py
+++ Code/PiCalculationWithSinInverseExpansion.py
@@ -1,21 +1,18 @@
 x = 1
-limit = 8
+limit = 22222
 seriesSum = x
-temp = 2
 
 for i in range(3, limit, 2):
     y = float(1)
 
-    for j in range(1, (i+1)):
+    for j in range(1, i):
         if j % 2 == 0:
             y /= j
-        elif j % 2 == 1:
+        else:
             y *= j
 
     y /= i
-    #seriesSum += (((i-2)/(temp*i)) * (x**i))
     seriesSum += (y * (x**i))
-    #temp *= 2
 
 
 seriesSum *= 2
